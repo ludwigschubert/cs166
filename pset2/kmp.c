1a37f312adfc81f1e7fbf6064642154d4636123f
Ludwig Schubert <git@ludwigschubert.de>
2016-04-17 17:08:37 -0700

    Initial creation and freeing of matcher. Potentially leaky.

--- pset2/kmp.c
+++ pset2/kmp.c
@@ -1,15 +1,36 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "kmp.h"
 
 struct matcher {
-  /* TODO: Fill this in! */
+    int pattern_length;
+    char* pattern;
+	int* suffix_links;
 };
 
 struct matcher* create_matcher_for(const char* pattern) {
-  /* TODO: Fill this in! */
+    struct matcher* aMatcher = (struct matcher*) malloc( sizeof(struct matcher) );
+
+    // copy string
+    aMatcher->pattern_length = strlen(pattern);
+    aMatcher->pattern = (char*) malloc(aMatcher->pattern_length * sizeof(char));
+    strcpy(aMatcher->pattern, pattern);
+
+    // create suffix_link array
+    aMatcher->suffix_links = (int*) malloc(aMatcher->pattern_length * sizeof(int));
+
+    // compute suffix_length_array
+    printf("create_matcher_for");
+    return aMatcher;
 }
 
 void destroy_matcher(struct matcher* matcher) {
-  /* TODO: Fill this in! */
+    free(matcher->pattern);
+    free(matcher->suffix_links);
+    free(matcher);
+    printf("destroy_matcher");
+    return;
 }
 
 void for_each_match(struct matcher* matcher,
