0242e2643d4c1f6eb63886763e673af39469bcb0
Ludwig Schubert <git@ludwigschubert.de>
2016-04-27 10:18:17 -0700

    Fix child() to always return correct child even in incomplete tree

--- pset3/fenwick-tree.c
+++ pset3/fenwick-tree.c
@@ -1,7 +1,9 @@
 #include "fenwick-tree.h"
 #include <stddef.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <assert.h>
 
 /*
  * "private" function declarations
@@ -10,7 +12,7 @@
 void recursive_increment(struct fenwick_tree* tree, size_t index, int amount, size_t node, bool went_left);
 int recursive_cumulative_frequency(const struct fenwick_tree* tree, size_t index, size_t node, bool went_left, int current_total);
 
-size_t child(size_t parent, bool left);
+size_t child(const struct fenwick_tree *tree, size_t parent, bool left);
 bool child_exists(const struct fenwick_tree* tree, size_t parent, bool left);
 
 struct fenwick_tree {
@@ -24,8 +26,8 @@ struct fenwick_tree* create_tree_of_size(size_t size) {
 
     // Allocate struct and internal arrays
     struct fenwick_tree* tree = (struct fenwick_tree*) malloc(sizeof(struct fenwick_tree));
-    tree->histogram     = (int*) malloc((size + 1)* sizeof(int));
-    tree->sub_tree_sums = (int*) malloc((size + 1)* sizeof(int));
+    tree->histogram     = (int*) malloc( (size + 1) * sizeof(int));
+    tree->sub_tree_sums = (int*) malloc( (size + 1) * sizeof(int));
 
     // Find root node; the largest power of two within our tree
     size_t root_node = 1;
@@ -53,6 +55,7 @@ void destroy_tree(struct fenwick_tree* tree) {
 }
 
 void increment(struct fenwick_tree* tree, size_t index, int amount) {
+    assert(0 < index && index <= tree->size);
     recursive_increment(tree, index, amount, tree->root_node, false);
 }
 
@@ -62,7 +65,8 @@ void recursive_increment(struct fenwick_tree* tree, size_t index, int amount, si
         tree->sub_tree_sums[node] += amount;
     } else { // continue descending
         bool go_left = index < node;
-        recursive_increment(tree, index, amount, child(node, go_left), go_left);
+        recursive_increment(tree, index, amount, child(tree, node, go_left), go_left);
+        assert(node <= tree->size);
         tree->sub_tree_sums[node] += amount;
     }
 
@@ -70,9 +74,6 @@ void recursive_increment(struct fenwick_tree* tree, size_t index, int amount, si
 
 int cumulative_frequency(const struct fenwick_tree* tree, size_t index) {
     int recursive_result = recursive_cumulative_frequency(tree, index, tree->root_node, false, 0);
-    if (recursive_result != naive_cumulative_frequency(tree, index)) {
-        ;
-    }
     return recursive_result;
 }
 
@@ -81,20 +82,20 @@ int recursive_cumulative_frequency(const struct fenwick_tree* tree, size_t index
         int local_total = current_total;
         local_total += tree->histogram[node];
         if (child_exists(tree, node, true)) {
-            local_total += tree->sub_tree_sums[child(node, true)];
+            local_total += tree->sub_tree_sums[child(tree, node, true)];
         }
         return local_total;
     } else { // continue descending
         bool go_left = index < node;
         if (go_left) {
-            return recursive_cumulative_frequency(tree, index, child(node, go_left), go_left, current_total);
+            return recursive_cumulative_frequency(tree, index, child(tree, node, go_left), go_left, current_total);
         } else {
             int local_total = current_total;
             local_total += tree->histogram[node];
             if (child_exists(tree, node, true)) {
-                local_total += tree->sub_tree_sums[child(node, true)];
+                local_total += tree->sub_tree_sums[child(tree, node, true)];
             }
-            return recursive_cumulative_frequency(tree, index, child(node, go_left), go_left, local_total);
+            return recursive_cumulative_frequency(tree, index, child(tree, node, go_left), go_left, local_total);
         }
     }
 }
@@ -105,19 +106,28 @@ int recursive_cumulative_frequency(const struct fenwick_tree* tree, size_t index
  */
 
 bool child_exists(const struct fenwick_tree* tree, size_t parent, bool left) {
-    size_t child_index = child(parent, true);
-    bool valid_index = 1 <= child_index <= tree->size;
+    size_t child_index = child(tree, parent, true);
+
+    bool valid_index = 1 <= child_index && child_index <= tree->size; // we ignore the 0'th index
     bool not_parent = child_index != parent;
-    return valid_index && not_parent;
+    bool not_negative = child_index > 0; // we ignore the 0'th index
+
+    return valid_index && not_parent && not_negative;
 }
 
-size_t child(size_t parent, bool left){
+size_t child(const struct fenwick_tree *tree, size_t parent, bool left) {
     size_t two_to_node_level = (parent & -parent) / 2; // magic; via pset
+    size_t potential_child_node;
     if (left) {
-        return parent - two_to_node_level;
+        potential_child_node = parent - two_to_node_level;
     } else {
-        return parent + two_to_node_level;
+        potential_child_node = parent + two_to_node_level;;
+        while(potential_child_node > tree->size){
+            two_to_node_level /= 2;
+            potential_child_node = parent + two_to_node_level;
+        }
     }
+    return potential_child_node;
 }
 
 //size_t left_child(size_t parent){
