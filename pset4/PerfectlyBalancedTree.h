eef228a36d307c4e1c0a8e7817ee9b4154110bb6
Ludwig Schubert <git@ludwigschubert.de>
2016-05-04 18:40:15 -0700

    Change PerfectlyBalancedTree to make tree in O(n)

--- pset4/PerfectlyBalancedTree.h
+++ pset4/PerfectlyBalancedTree.h
@@ -1,6 +1,7 @@
 #ifndef PerfectlyBalancedTree_Included
 #define PerfectlyBalancedTree_Included
 
+#include <assert.h>
 #include <stddef.h>
 #include <iostream>
 #include <vector>
@@ -31,27 +32,52 @@ public:
    * present in the tree.
    */
   bool contains(int key) const;
+
 private:
 
   class BinaryTreeNode {
   public:
+    int key;
+    BinaryTreeNode *left_child;
+    BinaryTreeNode *right_child;
+
     BinaryTreeNode(int key) {
       this->key = key;
       this->left_child = nullptr;
       this->right_child = nullptr;
     }
-    int key;
-    BinaryTreeNode *left_child;
-    BinaryTreeNode *right_child;
+
+    /**
+     * Recursively constructs a perfectly balanced complete search tree.
+     * This does not take keys as we're just assuming an 1...n-1 complete tree.
+     */
+    static BinaryTreeNode *make_tree(size_t left, size_t right) {
+
+      assert(left <= right);
+
+      if (left == right) {
+        return new BinaryTreeNode(left);
+      } else {
+        size_t pivot = (left + right + 1) / 2;
+
+        BinaryTreeNode *node = new BinaryTreeNode(pivot);
+
+        size_t next_right = pivot - 1;
+        if (next_right >= left) {
+          node->left_child = make_tree(left, next_right);
+        }
+
+        size_t next_left = pivot + 1;
+        if (next_left <= right) {
+          node->right_child = make_tree(next_left, right);
+        }
+
+        return node;
+      }
+    }
   };
 
   BinaryTreeNode *root;
-
-  /**
-   * Inserts the given key into the tree, by searching the tree and generating a new node.
-   * Duplicate values that are found during insertion are ignored.
-   */
-  void insert(int key);
 
   /* Fun with C++: these next two lines disable implicitly-generated copy
    * functions that would otherwise cause weird errors if you tried to
