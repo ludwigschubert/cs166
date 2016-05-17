06de5912de68d8f201aaf278dafa984469c3b393
Ludwig Schubert <git@ludwigschubert.de>
2016-05-16 23:03:46 -0700

    FInish LinearProbingHashTable

--- pset5/LinearProbingHashTable.cc
+++ pset5/LinearProbingHashTable.cc
@@ -1,6 +1,8 @@
 #include <cassert>
 #include "LinearProbingHashTable.h"
 
+const int TOMBSTONE = -1;
+
 LinearProbingHashTable::LinearProbingHashTable(size_t numBuckets, std::shared_ptr<HashFamily> family)
 {
   this->hashFunction = family->get();
@@ -16,10 +18,9 @@ void LinearProbingHashTable::insert(int data)
 {
   size_t index = this->index_for_data(data);
   while(this->buckets[index]) {
-    if(this->buckets[index] == data) {
-      return;
-    }
-    index = next_index(index);
+    if (this->buckets[index] == data) return; // found data; don't insert duplicate
+    if (this->buckets[index] == TOMBSTONE) break; // found TOMBSTONE, insert here
+    index = next_index(index); // continue scanning
   }
   this->buckets[index] = data;
 }
@@ -41,42 +42,28 @@ void LinearProbingHashTable::remove(int data)
   size_t index = this->index_for_data(data);
   while(this->buckets[index]) {
     if(this->buckets[index] == data) {
-      this->buckets[index] = 0;
-      break;
+      this->buckets[index] = TOMBSTONE;
+      return;
     }
     index = next_index(index);
   }
-  // repair "hole" by moving everything else back...
-  index = next_index(index);
-  while(this->buckets[index]) {
-    size_t correct_index = this->index_for_data(this->buckets[index]);
-    if (index == correct_index) {
-      return; // until something is hashed correctly...
-    }
-    size_t previous_index = this->previous_index(index);
-    this->buckets[previous_index] = this->buckets[index];
-    index = next_index(index);
-  } // or there is an empty slot
 }
 
-size_t LinearProbingHashTable::previous_index(size_t index) const
+inline size_t LinearProbingHashTable::previous_index(size_t index) const
 {
-  size_t previous_index;
   if (index == 0) {
-    previous_index = this->buckets.size() - 1;
+    return this->buckets.size() - 1;
   } else {
-    previous_index = index - 1;
+    return index - 1;
   }
-  assert(previous_index < this->buckets.size());
-  return previous_index;
 }
 
-size_t LinearProbingHashTable::next_index(size_t index) const
+inline size_t LinearProbingHashTable::next_index(size_t index) const
 {
   return ++index % this->buckets.size();
 }
 
-size_t LinearProbingHashTable::index_for_data(int data) const
+inline size_t LinearProbingHashTable::index_for_data(int data) const
 {
   size_t hash_value = this->hashFunction(data);
   size_t index = hash_value % this->buckets.size();
