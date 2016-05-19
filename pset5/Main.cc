b5ad47da22da06d2ec49f8b16e41d74ec9ac1199
Ludwig Schubert <git@ludwigschubert.de>
2016-05-19 11:52:41 -0700

    initial main.cc and timing.h

--- pset5/Main.cc
+++ pset5/Main.cc
@@ -41,39 +41,38 @@ int main() {
   std::cout << "  Cuckoo:         " << (checkCorrectness<CuckooHashTable>(allHashFamilies) ? "pass" : "fail") << std::endl;
   std::cout << std::endl;
 
-    /* Test linear probing variants. */
-    auto probingLoadFactors = {0.3, 0.5, 0.7, 0.9, 0.99};
+  /* Test linear probing variants. */
+  auto probingLoadFactors = {0.3, 0.5, 0.7, 0.9, 0.99};
 
-    std::cout << "#### Timing Linear Probing ####" << std::endl;
-    doAllReports<LinearProbingHashTable>(allHashFunctions, probingLoadFactors);
-    std::cout << "###########################" << std::endl;
-    std::cout << std::endl;
+  std::cout << "#### Timing Linear Probing ####" << std::endl;
+  doAllReports<LinearProbingHashTable>(allHashFunctions, probingLoadFactors);
+  std::cout << "###########################" << std::endl;
+  std::cout << std::endl;
+  
+  std::cout << "#### Timing Robin Hood ####" << std::endl;
+  doAllReports<RobinHoodHashTable>(allHashFunctions, probingLoadFactors);
+  std::cout << "###########################" << std::endl;
+  std::cout << std::endl;
+  
+  
+  /* Test chained hashing variants. */
+  auto chainedLoadFactors = {0.3, 0.5, 0.7, 0.9, 0.99, 2.0, 5.00};
+  
+  std::cout << "#### Timing Chained ####" << std::endl;
+  doAllReports<ChainedHashTable>(allHashFunctions, chainedLoadFactors);
+  std::cout << "###########################" << std::endl;
+  std::cout << std::endl;
+  
+  std::cout << "#### Timing Second-Choice ####" << std::endl;
+  doAllReports<SecondChoiceHashTable>(allHashFamilies, chainedLoadFactors);
+  std::cout << "###########################" << std::endl;
+  std::cout << std::endl;
+  
+  /* Test cuckoo hashing. */
+  auto cuckooLoadFactors = {0.2, 0.3, 0.4, 0.45, 0.47};
 
-    std::cout << "#### Timing Robin Hood ####" << std::endl;
-    doAllReports<RobinHoodHashTable>(allHashFunctions, probingLoadFactors);
-    std::cout << "###########################" << std::endl;
-    std::cout << std::endl;
-
-
-    /* Test chained hashing variants. */
-    auto chainedLoadFactors = {0.3, 0.5, 0.7, 0.9, 0.99, 2.0, 5.00};
-
-    std::cout << "#### Timing Chained ####" << std::endl;
-    doAllReports<ChainedHashTable>(allHashFunctions, chainedLoadFactors);
-    std::cout << "###########################" << std::endl;
-    std::cout << std::endl;
-
-    std::cout << "#### Timing Second-Choice ####" << std::endl;
-    doAllReports<SecondChoiceHashTable>(allHashFamilies, chainedLoadFactors);
-    std::cout << "###########################" << std::endl;
-    std::cout << std::endl;
-
-    /* Test cuckoo hashing. */
-    auto cuckooLoadFactors = {0.2, 0.3, 0.4, 0.45, 0.47};
-
-    std::cout << "#### Timing Cuckoo Hashing ####" << std::endl;
-    doAllReports<CuckooHashTable>(allHashFamilies, cuckooLoadFactors);
-    std::cout << "###########################" << std::endl;
-    std::cout << std::endl;
-
+  std::cout << "#### Timing Cuckoo Hashing ####" << std::endl;
+  doAllReports<CuckooHashTable>(allHashFamilies, cuckooLoadFactors);
+  std::cout << "###########################" << std::endl;
+  std::cout << std::endl;
 }
